f4342ff153dcff3f5057676997c46fa717a59dbc
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2021-02-02 17:16:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
-# code your #valid_move? method here
+def position_taken?(array, index)
+  
+  if(array[index] == "" || array[index] == nil || array[index] == " ")
+    return true
+  else 
+    return = false
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  if(position_taken(board, index) && index.between?(0, 8))
+    return true
+  else 
+    return false
+end
